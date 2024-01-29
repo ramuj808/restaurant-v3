459d1274848ffbb8dd6ae2275c2afdab0e9325d1
Jumar Pantoca <jpantoca@gmail.com>
2024-01-28 18:45:59 -1000

    Updated Header and include navbar links in the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,51 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
-const header = () => {
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <Navbar dark color="primary" sticky="top" expand="md">
       <NavbarBrand>
         <h1 className="mt-1">Kanak Attack Restaurant</h1>
       </NavbarBrand>
+
+      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+      <Collapse isOpen={menuOpen} navbar>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              <i className="fa fa-home fa-lg" /> Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/directory">
+              <i className="fa fa-list fa-lg" /> Menu
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/about">
+              <i className="fa fa-info fa-lg" /> About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/contact">
+              <i className="fa fa-address-card fa-lg" /> Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+        {/* <UserLoginForm /> */}
+      </Collapse>
     </Navbar>
   );
 };
 
-export default header;
+export default Header;
