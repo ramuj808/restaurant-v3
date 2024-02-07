56cfd5967a14a8b0a5a46b820438a060c0c5ece2
Jumar Pantoca <jpantoca@gmail.com>
2024-02-07 11:15:41 -1000

    Updated take out page with menu items and photos

--- src/pages/TakeOutPage.js
+++ src/pages/TakeOutPage.js
@@ -1,4 +1,9 @@
 import { Col, Row, Container, Button, Card } from "react-bootstrap";
+import locoMoco from "../assets/img/Loco-Moco-4-scaled.jpg";
+import kaluaPig from "../assets/img/Kalua-Pig-20.jpg";
+import shoyuChicken from "../assets/img/Shoyu-Chicken-1.jpg";
+import pokeNachos from "../assets/img/Poke-Nachos-9-683x1024.webp";
+import malasadas from "../assets/img/Malasadas-11-1365x2048.webp";
 
 const TakeOutPage = () => {
   return (
@@ -7,7 +12,7 @@ const TakeOutPage = () => {
       <Row className="mt-5">
         <Col>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="..." alt="loco moco" />
+            <Card.Img variant="top" src={locoMoco} alt="loco moco" />
             <Card.Body>
               <Card.Title>Loco Moco</Card.Title>
               <Card.Text>Beef patty and eggs all over rice</Card.Text>
@@ -17,7 +22,7 @@ const TakeOutPage = () => {
         </Col>
         <Col>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="..." alt="Kalua Pork" />
+            <Card.Img variant="top" src={kaluaPig} alt="Kalua Pork" />
             <Card.Body>
               <Card.Title>Kalua Pork</Card.Title>
               <Card.Text>Pork blah blah blah</Card.Text>
@@ -27,10 +32,31 @@ const TakeOutPage = () => {
         </Col>
         <Col>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="..." alt="Shoyu Chicken" />
+            <Card.Img variant="top" src={shoyuChicken} alt="Shoyu Chicken" />
             <Card.Body>
               <Card.Title>Shoyu Chicken</Card.Title>
               <Card.Text>Shoyu marinated chicken all over rice</Card.Text>
+              <Button variant="primary">Add To Cart</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={pokeNachos} alt="poke nachos" />
+            <Card.Body>
+              <Card.Title>Poke Nachos</Card.Title>
+              <Card.Text>Our signature poke over a bed of nachos</Card.Text>
+              <Button variant="primary">Add To Cart</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={malasadas} alt="malasadas" />
+            <Card.Body>
+              <Card.Title>Malasadas</Card.Title>
+              <Card.Text>Our Dessert</Card.Text>
               <Button variant="primary">Add To Cart</Button>
             </Card.Body>
           </Card>
